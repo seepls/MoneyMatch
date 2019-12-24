435515fe0e7daaf1b2e28eb0f4419a0535741862
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2019-12-24 09:52:28 +0800

    Create yhat.py

---
+++ churnRate/yhat.py
@@ -0,0 +1,33 @@
+## Looks some big shit .important.
+
+from yhat import Yhat,YhatModel,preprocess
+
+class ChurnModel(YhatModel):
+    # Type casts incoming data as a dataframe
+    @preprocess(in_type=pd.DataFrame,out_type=pd.DataFrame)
+    def execute(self,data):
+        # Collect customer meta data
+        response = data[['Area Code','Phone']]
+        charges = ['Day Charge','Eve Charge','Night Charge','Intl Charge']
+        response['customer_worth'] = data[charges].sum(axis=1)
+        # Convert yes no columns to bool
+        data[yes_no_cols] = data[yes_no_cols] == 'yes'
+        # Create feature space
+        X = data[features].as_matrix().astype(float)
+        X = scaler.transform(X)
+        # Make prediction
+        churn_prob = clf.predict_proba(X)
+        response['churn_prob'] = churn_prob[:,1]
+        # Calculate expected loss by churn
+        response['expected_loss'] = response['churn_prob'] * response['customer_worth']
+        response = response.sort('expected_loss', ascending=False)
+        # Return response DataFrame
+        return response
+
+yh = Yhat(
+    "e[at]yhathq.com",
+    "MY API KEY",
+    "http://cloud.yhathq.com/"
+)
+
+response = yh.deploy("PythonChurnModel",ChurnModel,globals())
